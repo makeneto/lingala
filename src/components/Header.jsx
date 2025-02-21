911621a8db69354b318aaa619fb48798cd40d5d7
Makene Neto <130509472+makeneto@users.noreply.github.com>
2025-02-21 12:27:28 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 function Header() {
   return (
     <header className='app-header'>
-      <img src='logo512.png' alt='Logo da LIngala Academy' />
       <h1>Lingala Quiz</h1>
     </header>
-  );
+  )
 }
 
 export default Header
